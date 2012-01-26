4a2e1334efe44ae2ad3aaf424d8fcc79e655a6e1
Bo Liu <bo658713@gmail.com>
2012-01-26 17:48:13 -0500

    Revert "Revert "Add internal variable for username, add ability to send message with your username. GUI does not handle this yet.""
    This reverts commit 93aa75a9f9d130b9e89bf33655a977d6f4e300e2.

--- src/synchronicity/syn_parsing.c
+++ src/synchronicity/syn_parsing.c
@@ -5,13 +5,14 @@
 // 8 characters for command, then data
 
 #define TYPE_LENGTH 8
-#define COMMAND_LENGTH ((int)(TYPE_LENGTH+sizeof(vlc_value_t)))
+#define COMMAND_LENGTH ((int)(TYPE_LENGTH+sizeof(vlc_value_t)+MESSAGE_LENGTH))
 
 const char* SynCommandTypeNames[] = {
   "play    ",
   "pause   ",
   "seek    ",
   "beat    ",
+  "mynameis",
   "error   "
 };
 
@@ -36,6 +37,12 @@ SynCommand CommandFromString(char* buffer, int length) {
 
   memcpy(&return_value.data, next_token, sizeof(return_value.data));
   next_token += sizeof(return_value.data);
+  
+  if (i == SYNCOMMAND_MYNAMEIS) {
+    memcpy(&return_value.message, next_token, sizeof(return_value.message));
+  }
+  next_token += sizeof(return_value.message);
+
   if (COMMAND_LENGTH != (next_token - buffer) ||
       buffer + length < next_token) {
     return_value.type = SYNCOMMAND_ERROR;
@@ -52,6 +59,7 @@ int StringFromCommand(SynCommand command, char* outbuffer, int length) {
     case SYNCOMMAND_PLAY:
     case SYNCOMMAND_PAUSE:
     case SYNCOMMAND_SEEK:
+    case SYNCOMMAND_MYNAMEIS:
     case SYNCOMMAND_BEAT:
       num_bytes = snprintf(outbuffer, length,
           "%s", SynCommandTypeNames[command.type]);
@@ -66,6 +74,9 @@ int StringFromCommand(SynCommand command, char* outbuffer, int length) {
       // error
       return -1;
       break;
+  }
+  if (command.type == SYNCOMMAND_MYNAMEIS) {
+    memcpy(outbuffer + TYPE_LENGTH + sizeof(command.data), &command.message, sizeof(command.message));
   }
   return COMMAND_LENGTH;
 }
