f1a895b316a8a89ac8b8c8b4358f30afca6c4918
Rémi Denis-Courmont <remi@remlab.net>
2012-03-11 16:27:07 +0200

    Linux DVB: fix format string
    (cherry picked from commit 42b8a557d6f0045c2a3a06a9ff457ce2a9cf7144)
    
    Conflicts:
    
    	modules/access/dtv/linux.c

--- modules/access/dtv/linux.c
+++ modules/access/dtv/linux.c
@@ -67,7 +67,7 @@ static int dvb_open_node (int dir, const char *type, unsigned dev, int flags)
     int fd;
     char path[strlen (type) + 4];
 
-    snprintf (path, sizeof (path), "%s%"PRIu8, type, dev);
+    snprintf (path, sizeof (path), "%s%u", type, dev);
     fd = openat (dir, path, flags|O_CLOEXEC);
     if (fd != -1)
         fcntl (fd, F_SETFL, fcntl (fd, F_GETFL) | O_NONBLOCK);
