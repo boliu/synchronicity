a2a951f3fa1f568bf56ade05e81edcc1a199faab
Jean-Baptiste Kempf <jb@videolan.org>
2012-03-12 19:20:17 +0100

    Qt4: do not delete useful text (cherry picked from commit c26ef9bc31a6ecd06dd4272e9139e6611e2e1294)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/gui/qt4/components/preferences_widgets.cpp
+++ modules/gui/qt4/components/preferences_widgets.cpp
@@ -1517,8 +1517,11 @@ bool KeySelectorControl::eventFilter( QObject *obj, QEvent *e )
     }
     else if( keyEv->key() == Qt::Key_Delete )
     {
-        aTable->currentItem()->setText( aTable->currentColumn(), NULL );
-        aTable->currentItem()->setData( aTable->currentColumn(), Qt::UserRole, QVariant() );
+        if( aTable->currentColumn() != 0 )
+        {
+            aTable->currentItem()->setText( aTable->currentColumn(), NULL );
+            aTable->currentItem()->setData( aTable->currentColumn(), Qt::UserRole, QVariant() );
+        }
         return true;
     }
     else
