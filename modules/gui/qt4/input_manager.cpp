c17e2e7f0e530c4acde584391d8e86b8f17d628a
Jean-Baptiste Kempf <jb@videolan.org>
2012-04-06 13:29:13 +0200

    Qt: fix semantics and behaviour for chapters buttons
    No sense to display chapter buttons with 1title&1chapter
    (cherry picked from commit 2059bea41c0ea723466ccbb3e9d9550b26e2e2ee)
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/gui/qt4/input_manager.cpp
+++ modules/gui/qt4/input_manager.cpp
@@ -426,15 +426,14 @@ void InputManager::UpdateNavigation()
 
     if( val.i_int > 0 )
     {
-        emit titleChanged( true );
-        msg_Dbg( p_intf, "Title %"PRId64, val.i_int );
         /* p_input != NULL since val.i_int != 0 */
         var_Change( p_input, "chapter", VLC_VAR_CHOICESCOUNT, &val2, NULL );
-        emit chapterChanged( (val2.i_int > 1) || ( val2.i_int > 0 && val.i_int > 1 ) );
-        msg_Dbg( p_intf, "Chapter: %"PRId64, val2.i_int );
+
+        emit titleChanged( val.i_int > 1 );
+        emit chapterChanged( val2.i_int > 1 );
     }
     else
-        emit titleChanged( false );
+        emit chapterChanged( false );
 }
 
 void InputManager::UpdateStatus()
