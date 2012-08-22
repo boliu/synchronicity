fc6d6a0e5d394dd3538ff72e216ed53404ba894a
Felix Paul Kühne <fkuehne@videolan.org>
2012-08-22 11:02:57 +0200

    macosx: update check for Lion and add another one for Mountain Lion (cherry picked from commit 0849d6e510d6e46a9a90f24d96fc800dfbdeb998)

--- modules/gui/macosx/CompatibilityFixes.h
+++ modules/gui/macosx/CompatibilityFixes.h
@@ -27,7 +27,8 @@
 #pragma OS detection code
 #define OSX_LEOPARD (NSAppKitVersionNumber < 1038 && NSAppKitVersionNumber >= 949)
 #define OSX_SNOW_LEOPARD (NSAppKitVersionNumber < 1115 && NSAppKitVersionNumber >= 1038)
-#define OSX_LION NSAppKitVersionNumber >= 1115.2
+#define OSX_LION (NSAppKitVersionNumber < 1162 && NSAppKitVersionNumber >= 1115.2)
+#define OSX_MOUNTAIN_LION NSAppKitVersionNumber >= 1162
 
 #pragma mark -
 #pragma Fixes for OS X Leopard (10.5)
